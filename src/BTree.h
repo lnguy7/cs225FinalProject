8f39d45c64e65f7b1f39c4d37a7e65a0b68cabf9
Bella Cruz <icruz22@illinois.edu>
2022-11-25 17:46:29 +0000

    Added documentation to the Data class in Data.h and updated the documentation for BTree.h

--- src/BTree.h
+++ src/BTree.h
@@ -22,67 +22,74 @@
            */
            BTreeNode();
 
+           /**
+           * Constructs a BTreeNode with the provided streamer's id and their corresponding feature data. 
+           * 
+           * TODO: Add a parameter for the streamer's mutual followers to initialize, the member variable "mutual_followers". 
+           * 
+           * @param streamer_id_and_feature_data vector that is used to initialize the BTreeNode member variable, "streamer_data".
+           */
            BTreeNode(vector<string> streamer_id_and_feature_data);
 
-           /*
+           /**
            * Returns the streamer id.
-           *
+           * 
            * @return numeric_id.
            */
            string getId();
 
-           /*
+           /**
            * Returns the number of streamer views.
-           *
+           * 
            * @return views.
            */
            string getStreamerViews();
 
-           /*
+           /**
            * Returns the number of the streamer's mature viewings.
-           *
+           * 
            * @return mature.
            */
            string getStreamerMatureViewings();
 
-           /*
+           /**
            * Returns the how long it's been since the streamer created their account.
-           *
+           * 
            * @return life_time.
            */
            string getLifeTime();
 
-           /*
+           /**
            * Returns the date the streamer created their account.
-           *
+           * 
            * @return created_at.
            */
            string getAccountCreatedAt();
 
-           /*
+           /**
            * Returns the most recent date that the streamer updated their account.
-           *
+           * 
            * @return updated_at.
            */
            string getAccountUpdatedAt();
 
-           /*
+           /**
            * Returns whether the streamer's account is a dead account.
            * 
            * @return dead_account.
            */
            string getDeadAccount();
 
-           /*
+           /**
            * Returns the primary language of the streamer.
-           *
+           * 
            * @return language.
            */
            string getLanguage();
 
-           /*
+           /**
            * Returns the streamer's affiliation.
-           *
+           * 
            * @return affiliate.
            */
            string getAffiliate();
@@ -106,24 +113,22 @@
       */
       BTree();
 
-      /*
+      /**
       * Constructs a graphical represenation of all the streamers, their feature data, and connections.
-      *
+      * 
       * @param streamers all the streamers, their feature data, and connections.
       */
       BTree(vector<BTreeNode> streamers);
 
-      /*
+      /**
       * Returns a graphical represenation of all the streamers.
-      *
+      * 
       * @return the vector representation of a B-Tree.
       */
       vector<BTreeNode> getBTree();
 
     private:
-      /*
-      * This vector contains the entire B-Tree.
-      */
+      /* This vector contains the entire B-Tree. */
       vector<BTreeNode> b_tree;
 
  };
--- src/Data.h
+++ src/Data.h
@@ -21,21 +21,68 @@ using namespace std;
 
 class Data {
     public:
+      /*
+      * The default constructor for the Data class.
+      */
       Data();
 
+      /**
+      * Returns the b-tree structure that the data is organized in.
+      * 
+      * @return data_b_tree.
+      */
       BTree get_data_b_tree();
 
+      /**
+      * Given a filename to a CSV-formatted text file, create a 2D vector of strings where each row
+      * in the text file is a row in the V2D and each comma-separated value is stripped of whitespace
+      * and stored as its own string.
+      *  
+      * @param filename The filename of a CSV-formatted text file. 
+      * @return The vector of vectors of strings imported from filename
+      */
       vector<vector<string>> file_to_nested_vector(const std::string& filename);
 
+      /**
+      * Loops through each row of the nested vector and creates a BTreeNode for each row (each row represents a streamer).
+      * The BTree of the dataset is then constructed from the vector of nodes created.
+      * 
+      * @param data_in_vector_structure the nested vector of strings that holds the data from the file, "large_twitch_features.csv".
+      */
       void create_B_tree(vector<vector<string>> data_in_vector_structure);
 
 
     private:
+      /**
+      * Takes a filename and reads in all the text from the file
+      * 
+      * @param filename The name of the file that will fill the string
+      * @return A string containing the whole text of the given file
+      */
       string data_to_string(const string& filename);
+
+      /**
+      * Given a string and character, split the string by the character and store in a vector
+      * A 'split' string treats the character as a breakpoint and creates a separate item
+      * in the output vector for each substring. 
+      *  
+      * @param string_one The string being split
+      * @param sep The separator character
+      * @param fields The string vector storing all substrings created by the split
+      * @return The number of substrings created
+      */
+      int split_string(const string& string_one, char sep, vector<string>& fields);
+
+      /**
+      * Functions for the removal of whitespace to the left or right of a string (or both)
+      *
+      * @param string The string being processed
+      * @return The processed string
+      */
       string trim_right_side(const string& string);
       string trim_left_side(const string& string);
       string trim_all(const string& string);
-      int split_string(const string& string_one, char sep, vector<string>& fields);
-
+      
+      /** Private member variable that holds the graphical representation of the data. */
       BTree data_b_tree;
 };
